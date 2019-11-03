5ba80872bbe0425e1a66b2454e82c62b5fd93599
Aaron Bonetti <AaronMik Bonetti@gmail.com>
2019-11-02 23:19:29 -0400

    changed style of order page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,9 +1,9 @@
-import React, { Component }from "react"
-import { connect } from "react-redux"
-import AddOns from "./Add-Ons/AddOns"
-import "./order.scss"
-import {addItemToCart} from "../../actions/action-creators";
-import { Link } from "react-router-dom"
+import React, { Component }from 'react'
+import { connect } from 'react-redux'
+import AddOns from './Add-Ons/AddOns'
+import './order.scss'
+import {addItemToCart} from '../../actions/action-creators';
+import { Link } from 'react-router-dom'
 
 
 
@@ -13,9 +13,9 @@ class Order extends Component{
         this.state= {
             
             addOns:[
-                "Ketchup","Mayo","Mustard","Pickles","Onions"
+                'Ketchup','Mayo','Mustard','Pickles','Onions'
             ],
-            cookingInstructions:"",
+            cookingInstructions:'',
             Ketchup:false,
             Mayo:false,
             Mustard:false,
@@ -79,10 +79,10 @@ class Order extends Component{
 handleChange(e){
     const {name,value,type,checked}= e.target
     
-    type === "checkbox" ? this.setState(() => {
+    type === 'checkbox' ? this.setState(() => {
         return{   
         //I set the value of name to either false or the name so I could more easily parse the data sent to the store.       
-        [name]: checked  ? name : ""            
+        [name]: checked  ? name : ''            
     }
     }):
     this.setState({        
@@ -126,24 +126,24 @@ handleSubmit(){
 render(){
     console.log(this.state)
 const addOns= this.state.addOns.map(addOn=> 
-    <AddOns className="add-ons" key={addOn} name={addOn} onChange={this.handleChange} defaultChecked={this.handleDefaultChecked(addOn)}/>    
+    <AddOns className='add-ons' key={addOn} name={addOn} onChange={this.handleChange} defaultChecked={this.handleDefaultChecked(addOn)}/>    
 )
 
 
 const burgerName= this.props.burger[0].name
 
     return(
-        <div className="order-page-container">
-            <div className="order-container">
-                <h1 className="order-header">{burgerName}</h1>
-                <form className="order-form"onChange={this.handleChange}>
+        <div className='order-page-container'>
+            <div className='order-container'>
+                <h1 className='order-header'>{burgerName}</h1>
+                <form className='order-form'onChange={this.handleChange}>
                     <h2>Add Ons</h2>
                     {addOns}
-                    <h2 className="cooking-instructions">Cooking Instructions</h2>
-                    <textarea type="textarea" name="cookingInstructions" value={this.state.cookingInstructions} onChange={this.handleChange}/>
-                    <div className="button-container">
-                    <Link to="/Cart"><button onClick={this.handleSubmit}>Add To Cart</button></Link>
-                    <Link to="/menu"><button >Cancel</button></Link>
+                    <h2 className='cooking-instructions'>Cooking Instructions</h2>
+                    <textarea type='textarea' name='cookingInstructions' value={this.state.cookingInstructions} onChange={this.handleChange}/>
+                    <div className='button-container'>
+                    <Link to='/Cart' className='cart-link'><button onClick={this.handleSubmit}>Add To Cart</button></Link>
+                    <Link to='/menu' className='cart-link'><button >Cancel</button></Link>
                     </div>
                 </form>     
             </div>
